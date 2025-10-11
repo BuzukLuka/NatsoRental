3eb445d974353546dbc5479fd00e5ba1214d4b83
Bilguun <enkhbil2026@gmail.com>
2025-10-12 04:14:53 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "cdn.example.com" }, // add your real host
       { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "15-223-51-206.sslip.io" },
       { protocol: "http", hostname: "localhost" },
     ],
   },
